54d3116f840b616b7761c835198ea8705067b1dc
Darek <dareczek06@gmail.com>
2024-08-07 11:36:47 +0200

    feature: Added 3d component for the letter

---
+++ src/components/canvas/LetterCanvas.tsx
@@ -0,0 +1,43 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "./Loader";
+
+const Letter = () => {
+  const letterModel = useGLTF('./letter/scene.gltf')
+  return (
+    <primitive
+      object={letterModel.scene}
+      scale={3}
+      position-y={0}
+      rotation-y={0}
+    />
+  );
+};
+
+const LetterCanvas = () => {
+
+  return (
+    <Canvas shadows frameloop="demand" gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 1,
+        far: 50,
+        position: [-4, 3, 6]
+      }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        <Letter />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+export default LetterCanvas
